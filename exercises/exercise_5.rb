666d4852c4aa0864fad9a84d9935302fe8290763
Jeremy Huot <logflume@live.com>
2020-08-18 03:36:33 +0000

    Output all stores revenue, each stores average annual revenue, and all the stores doing 1M or more per year

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+@stores = Store.count
+@select = Store.where("annual_revenue >= 1000000")
+
+puts @total_revenue
+puts @total_revenue / @stores
+puts @select.length
