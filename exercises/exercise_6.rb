76e36735161d4ffa5f6531386f4470687b7852a7
Jeremy Huot <logflume@live.com>
2020-08-18 14:52:39 +0000

    Add relationship between Store and Employee models. Create 6 employees and add to the employees table

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Molly", last_name: "Blake", hourly_rate: 40)
+@store2.employees.create(first_name: "Keith", last_name: "Helc", hourly_rate: 20)
+@store1.employees.create(first_name: "Faraday", last_name: "Jones", hourly_rate: 90)
+@store2.employees.create(first_name: "Basil", last_name: "Smith", hourly_rate: 50)
